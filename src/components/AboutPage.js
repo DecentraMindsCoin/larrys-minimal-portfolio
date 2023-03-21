5bdd344f506ee270faec11c225d0f836b9955a24
DecentraMindsCoin <decentraminds@gmail.com>
2023-03-21 01:18:51 -0700

    add dependencies and alter main

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,40 +1,38 @@
-import React from 'react'
-import styled, { keyframes, ThemeProvider } from 'styled-components'
-import {DarkTheme} from './Themes';
-
-
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
-import ParticleComponent from '../subComponents/ParticleComponent';
-import BigTitle from '../subComponents/BigTitlte'
-import astronaut from '../assets/Images/spaceman.png'
+import React from "react";
+import styled, { keyframes, ThemeProvider } from "styled-components";
+import { DarkTheme } from "./Themes";
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
+import BigTitle from "../subComponents/BigTitlte";
+import astronaut from "../assets/Images/spaceman.png";
 
 const Box = styled.div`
-background-color: ${props => props.theme.body};
-width: 100vw;
-height:100vh;
-position: relative;
-overflow: hidden;
-`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
 const float = keyframes`
 0% { transform: translateY(-10px) }
 50% { transform: translateY(15px) translateX(15px) }
 100% { transform: translateY(-10px) }
 
-`
+`;
 const Spaceman = styled.div`
-position: absolute;
-top: 10%;
-right: 5%;
-width: 20vw;
-animation: ${float} 4s ease infinite;
-img{
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  width: 20vw;
+  animation: ${float} 4s ease infinite;
+  img {
     width: 100%;
     height: auto;
-}
-`
-const Main =  styled.div`
+  }
+`;
+const Main = styled.div`
   border: 2px solid ${(props) => props.theme.text};
   color: ${(props) => props.theme.text};
   padding: 2rem;
@@ -46,43 +44,40 @@ const Main =  styled.div`
   justify-content: center;
   align-items: center;
   font-size: calc(0.6rem + 1vw);
- backdrop-filter: blur(4px);
-  
+  backdrop-filter: blur(4px);
+
   position: absolute;
   left: calc(5rem + 5vw);
   top: 10rem;
-  font-family: 'Ubuntu Mono', monospace;
+  font-family: "Ubuntu Mono", monospace;
   font-style: italic;
-`
-
-
-
+`;
 
 const AboutPage = () => {
-    return (
-        <ThemeProvider theme={DarkTheme}>
-<Box>
-
-<LogoComponent theme='dark'/>
-<SocialIcons theme='dark'/>
-<PowerButton />
-<ParticleComponent theme='dark' />
-
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <ParticleComponent theme="dark" />
         <Spaceman>
-            <img src={astronaut} alt="spaceman" />
-        </Spaceman>    
+          <img src={astronaut} alt="spaceman" />
+        </Spaceman>
         <Main>
-        I believe progressing toward the future includes creating unique experiences using the most advanced programming technologies and design tools. As a self taught REACT JAVASCRIPT DEVELOPER, I am able to create unique things using Javascript, HTML, & CSS using frameworks and libraries like React, Node & Next.js. I am just a passionate coder & creator with various skills and abilities looking to work with a great team!
+          I believe progressing toward the future includes creating unique
+          experiences using the most advanced programming technologies and
+          design tools. As a self taught REACT JAVASCRIPT DEVELOPER, I am able
+          to create unique things using Javascript, HTML, & CSS using frameworks
+          and libraries like React, Node & Next.js. I am just a passionate coder
+          & creator with various skills and abilities looking to work with a
+          great team!
         </Main>
 
         <BigTitle text="ABOUT" top="10%" left="5%" />
+      </Box>
+    </ThemeProvider>
+  );
+};
 
-
-        </Box>
-
-        </ThemeProvider>
-        
-    )
-}
-
-export default AboutPage
+export default AboutPage;
